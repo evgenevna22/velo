805001dfad5cc0c757c973f73f6d8f618b8eb82b
Tatiana <tanya.evgenevna@gmail.com>
2018-05-27 23:40:23 +0300

    created modal feedback form

--- app/js/common.js
+++ app/js/common.js
@@ -20,3 +20,36 @@ $(document).ready(function () {
         }
     );
 });
+
+var btnsFeedback = document.querySelectorAll('.btn__feedback');
+var modal = document.querySelector('.modal');
+var modalOverlay = document.querySelector('.modal-overlay');
+var close = document.querySelector('.modal__close');
+var body = document.querySelector('body');
+
+for (var i = 0; i < btnsFeedback.length; i++) {
+    btnsFeedback[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        modal.classList.add('opened');
+        modalOverlay.classList.add('opened');
+        body.classList.add('modal-open');
+    });
+}
+
+close.addEventListener('click', function (event) {
+   event.preventDefault();
+   modal.classList.remove('opened');
+   modalOverlay.classList.remove('opened');
+   body.classList.remove('modal-open');
+});
+
+window.addEventListener('keydown', function(event) {
+    if (event.keyCode === 27) {
+        if (modal.classList.contains('opened') && modalOverlay.classList.contains('opened') && body.classList.contains('modal-open')) {
+            modal.classList.remove('opened');
+            modalOverlay.classList.remove('opened');
+            body.classList.remove('modal-open');
+
+        }
+    }
+});
